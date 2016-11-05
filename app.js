0942c8ac0fa6b2f778809081800bd8e507e00985
gusev-r <gusevrv0o@gmail.com>
2016-11-05 06:52:05 -0700

    error handling(use domains)

--- app.js
+++ app.js
@@ -5,9 +5,56 @@ var body_parser = require('body-parser').urlencoded({extended: true});
 var formidable = require('formidable');
 var cookie_parser = require('cookie-parser');
 var express_session = require('express-session');
+var http = require('http');
 const util = require('util');
 var app = express();
 app.set('port', process.env.PORT || 30000);
+
+
+// use domains for better error handling
+app.use(function(req, res, next){
+    // create a domain for this request
+    var domain = require('domain').create();
+    // handle errors on this domain
+    domain.on('error', function(err){
+        console.error('ПЕРЕХВАТ ОШИБКИ ДОМЕНА\n', err.stack);
+        try {
+            // failsafe shutdown in 5 seconds
+            setTimeout(function(){
+                console.error('Отказобезобасный сотанов');
+                process.exit(1);
+            }, 5000);
+
+            // disconnect from the cluster
+            var worker = require('cluster').worker;
+            if(worker) worker.disconnect();
+
+            // stop taking new requests
+            server.close();
+
+            try {
+                // attempt to use Express error route
+                next(err);
+            } catch(error){
+                // if Express error route failed, try
+                // plain Node response
+                console.error('Сбой механизмоа обработки ошибок Express.\n', error.stack);
+                res.statusCode = 500;
+                res.setHeader('content-type', 'text/plain');
+                res.end('Server error.');
+            }
+        } catch(error){
+            console.error('Unable to send 500 response.\n', error.stack);
+        }
+    });
+
+    // add the request and response objects to the domain
+    domain.add(req);
+    domain.add(res);
+
+    // execute the rest of the request chain in the domain
+    domain.run(next);
+});
 
 // logging
 switch(app.get('env')){
@@ -27,6 +74,16 @@ app.use(function (req, res, next) {
         console.log('Запрос обрабатывает исполнитель номер - %d', cluster.worker.id);
     };
     next();
+})
+
+app.get('/fail', function (req, res) {
+    throw new Error('Нет!');
+})
+
+app.get('/epic-fail', function (req, res) {
+    process.nextTick(function () {
+        throw new Error('Бабах!');
+    })
 })
 
 app.use(cookie_parser(credentials.cookieSecret));
@@ -57,7 +114,7 @@ app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
 
-app.use(body_parser); 
+app.use(body_parser);
 
 function getWeatherData() {
     return {
@@ -195,19 +252,19 @@ app.get('/tours/request-group-rate', function (req, res) {
 app.use(express.static( __dirname + '/public'));
 
 app.use(function (req, res) {
-    res.status(404);
-    res.render('404');
+    res.status(404).render('404');
 });
 
 app.use(function (err, req, res, next) {
     console.log(err.stack);
-    res.status(500);
-    res.render('500');
+    res.status(500).render('500');
 });
 
 
+var server;
+
 function startServer() {
-    app.listen(app.get('port'), function () {
+    server = http.createServer(app).listen(app.get('port'), function(){
         console.log( 'Express started in ' + app.get('env') +
             ' mode on http://localhost:' + app.get('port') +
             '; press Ctrl-C to terminate.' );
